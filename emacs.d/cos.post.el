b1acf7a9a2d54279012b12025d37e82c0fc12438
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2015-02-04 14:33:28 -0800

    refresh emacs config

--- emacs.d/cos.post.el
+++
@@ -1,9 +0,0 @@
-(menu-bar-mode -1)
-
-(setq linum-format
-     (lambda (line)
-       (propertize
-        (format
-         (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
-          (concat "%" (number-to-string w) "d ")) line
-          ) 'face 'linum)))
