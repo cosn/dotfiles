b7064f66d3d9b2d9e074e99f7cfea06654a12a5d
Cosmin Nicolaescu <cos@cosn.dev>
2024-04-25 15:57:44 -0700

    Update test keymaps since it broke lspconfig

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -102,11 +102,11 @@ return {
     'vim-test/vim-test',
     event = "VeryLazy",
     config = function()
-      vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
-      vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
-      vim.keymap.set("n", "<leader>a", ":TestSuite<CR>")
-      vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
-      vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
+      vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>")
+      vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+      vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+      vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+      vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
     end,
   },
 }
