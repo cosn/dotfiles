02453d14a81fa2efee747d7f48740cc7800b0686
Cosmin Nicolaescu <cos@cosn.dev>
2024-04-26 09:36:13 -0700

    fix gitsigns

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -5,6 +5,7 @@ return {
   },
   {
     'lewis6991/gitsigns.nvim',
+    event = "VeryLazy",
     opts = {
       signs = {
         add = { text = '+' },
@@ -12,55 +13,34 @@ return {
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
-          else
-            gitsigns.nav_hunk 'next'
-          end
-        end, { desc = 'Jump to next git [c]hange' })
-
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
-          else
-            gitsigns.nav_hunk 'prev'
-          end
-        end, { desc = 'Jump to previous git [c]hange' })
-
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis '@'
-        end, { desc = 'git [D]iff against last commit' })
-
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-        map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
-        end,
       },
     },
+    config = function(_, opts)
+      local gitsigns = require('gitsigns')
+      gitsigns.setup(opts)
+
+      vim.keymap.set('v', '<leader>ghs', function()
+        gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = '[g]it [h]unk [s]tage' })
+      vim.keymap.set('v', '<leader>ghr', function()
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = '[g]it [h]unk [r]eset' })
+
+      vim.keymap.set('n', '<leader>ghs', gitsigns.stage_hunk, { desc = '[g]it [s]tage hunk' })
+      vim.keymap.set('n', '<leader>ghr', gitsigns.reset_hunk, { desc = '[g]it [r]eset hunk' })
+      vim.keymap.set('n', '<leader>ghS', gitsigns.stage_buffer, { desc = '[g]it [S]tage buffer' })
+      vim.keymap.set('n', '<leader>ghu', gitsigns.undo_stage_hunk, { desc = '[g]it [u]ndo stage hunk' })
+      vim.keymap.set('n', '<leader>ghR', gitsigns.reset_buffer, { desc = '[g]it [R]eset buffer' })
+      vim.keymap.set('n', '<leader>ghp', gitsigns.preview_hunk, { desc = '[g]it [p]review hunk' })
+      vim.keymap.set('n', '<leader>ghb', gitsigns.blame_line, { desc = '[g]it [b]lame line' })
+      vim.keymap.set('n', '<leader>ghd', gitsigns.diffthis, { desc = '[g]it [d]iff against index' })
+      vim.keymap.set('n', '<leader>ghD', function()
+        gitsigns.diffthis '@'
+      end, { desc = '[g]it [D]iff against last commit' })
+
+      vim.keymap.set('n', '<leader>gtb', gitsigns.toggle_current_line_blame, { desc = '[git] [t]oggle git show [b]lame line' })
+      vim.keymap.set('n', '<leader>gtD', gitsigns.toggle_deleted, { desc = '[g]it [t]oggle git show [d]eleted' })
+    end,
   },
   {
     "kdheepak/lazygit.nvim",
