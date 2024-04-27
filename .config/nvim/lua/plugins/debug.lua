963dbf727f619976530fc9ee871de2678b1a9517
Cosmin Nicolaescu <cos@cosn.dev>
2024-04-26 18:42:58 -0700

    minor refactoring

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -90,12 +90,13 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("trouble").setup {}
-      vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-      vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-      vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-      vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-      vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+      local trouble = require("trouble")
+      trouble.setup {}
+      vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+      vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
+      vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
+      vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+      vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
     end,
   },
   {
--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,7 +1,6 @@
 return {
   {
     "stevearc/conform.nvim",
-    lazy = false,
     keys = {
       {
         "<leader>f",
