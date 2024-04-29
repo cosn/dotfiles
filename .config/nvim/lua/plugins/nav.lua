9427b0a33126ce18222c8511aec1628616a925f8
Cosmin Nicolaescu <cos@cosn.dev>
2024-04-29 13:19:11 -0700

    show hidden paths in oil

--- .config/nvim/lua/plugins/nav.lua
+++ .config/nvim/lua/plugins/nav.lua
@@ -45,6 +45,9 @@ return {
     'stevearc/oil.nvim',
     opts = {
       skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
