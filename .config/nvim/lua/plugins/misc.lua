091ade0e7c9672428967d5a4133e0e9077f146f3
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-22 11:20:53 -0700

    add nvim plugin

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,25 +1,25 @@
 return {
   {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
+    "folke/which-key.nvim",
+    event = "VimEnter",
     config = function()
-      local wk = require 'which-key'
-      wk.setup {}
+      local wk = require("which-key")
+      wk.setup({})
 
       -- Document existing key chains
-      wk.register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
+      wk.register({
+        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+        ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+        ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+      })
       -- visual mode
       wk.register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+        ["<leader>h"] = { "Git [H]unk" },
+      }, { mode = "v" })
     end,
   },
   {
@@ -27,10 +27,20 @@ return {
     event = "VeryLazy",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
   },
   {
     "folke/zen-mode.nvim",
-     opts = {},
+    opts = {},
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      restriction_mode = "hint",
+    },
   },
 }
