c6091a19d4c53f32bd002022e4c7e5b5783466ef
Cosmin Nicolaescu <cos@cosn.dev>
2024-05-24 12:23:29 -0700

    update nvim

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -1,18 +1,20 @@
 return {
   {
-    'mfussenegger/nvim-dap',
+    "mfussenegger/nvim-dap",
     dependencies = {
-      'rcarriga/nvim-dap-ui',
-      'nvim-neotest/nvim-nio',
-      'williamboman/mason.nvim',
-      'jay-babu/mason-nvim-dap.nvim',
-      'mxsdev/nvim-dap-vscode-js',
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "jay-babu/mason-nvim-dap.nvim",
+      "mxsdev/nvim-dap-vscode-js",
+      "theHamsta/nvim-dap-virtual-text",
     },
     config = function()
-      local dap = require 'dap'
-      local dapui = require 'dapui'
+      local dap = require("dap")
+      local dapui = require("dapui")
+      require("nvim-dap-virtual-text").setup({})
 
-      require('mason-nvim-dap').setup {
+      require("mason-nvim-dap").setup({
         automatic_setup = true,
 
         -- You can provide additional configuration to the handlers,
@@ -22,48 +24,48 @@ return {
         -- You'll need to check that you have the required things installed
         -- online, please don't ask me how to install them :)
         ensure_installed = {
-          'pwa-node',
+          "pwa-node",
         },
-      }
+      })
 
       -- Basic debugging keymaps, feel free to change to your liking!
-      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-      vim.keymap.set('n', '<leader>B', function()
-        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end, { desc = 'Debug: Set Breakpoint' })
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+      vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+      vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
+      vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "Debug: Set Breakpoint" })
 
       -- For more information, see |:help nvim-dap-ui|
-      dapui.setup {
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      dapui.setup({
+        icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
         controls = {
           icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-            disconnect = '⏏',
+            pause = "⏸",
+            play = "▶",
+            step_into = "⏎",
+            step_over = "⏭",
+            step_out = "⏮",
+            step_back = "b",
+            run_last = "▶▶",
+            terminate = "⏹",
+            disconnect = "⏏",
           },
         },
-      }
+      })
 
       -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+      vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 
-      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+      dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+      dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+      dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
-      require("dap-vscode-js").setup {
-        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
-      }
+      require("dap-vscode-js").setup({
+        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+      })
 
       for _, language in ipairs({ "typescript", "javascript" }) do
         dap.configurations[language] = {
@@ -78,7 +80,7 @@ return {
             type = "pwa-node",
             request = "attach",
             name = "Attach",
-            processId = require'dap.utils'.pick_process,
+            processId = require("dap.utils").pick_process,
             cwd = "${workspaceFolder}",
           },
         }
@@ -91,16 +93,26 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local trouble = require("trouble")
-      trouble.setup {}
-      vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
-      vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
-      vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
-      vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
-      vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
+      trouble.setup({})
+      vim.keymap.set("n", "<leader>xx", function()
+        trouble.toggle()
+      end)
+      vim.keymap.set("n", "<leader>xd", function()
+        trouble.toggle("document_diagnostics")
+      end)
+      vim.keymap.set("n", "<leader>xq", function()
+        trouble.toggle("quickfix")
+      end)
+      vim.keymap.set("n", "<leader>xl", function()
+        trouble.toggle("loclist")
+      end)
+      vim.keymap.set("n", "gR", function()
+        trouble.toggle("lsp_references")
+      end)
     end,
   },
   {
-    'vim-test/vim-test',
+    "vim-test/vim-test",
     event = "VeryLazy",
     dependencies = {
       "preservim/vimux",
