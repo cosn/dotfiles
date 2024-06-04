efdf5eb20c38d1d1b060cc651280da4f8c288427
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-04 08:47:23 -0700

    give supermaven a try

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -21,6 +21,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
       "onsails/lspkind.nvim",
+      "supermaven-inc/supermaven-nvim",
     },
     config = function()
       -- See `:help cmp`
@@ -77,6 +78,7 @@ return {
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "path" },
+          { name = "supermaven" },
         },
         formatting = {
           format = lspkind.cmp_format({
@@ -86,7 +88,7 @@ return {
             -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
             ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
             show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
+            symbol_map = { Supermaven = "" },
             -- The function below will be called before any actual modifications from lspkind
             -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
             -- before = function (entry, vim_item)
@@ -100,6 +102,18 @@ return {
   },
   {
     "github/copilot.vim",
+    enabled = false,
     event = "VeryLazy",
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = true,
+    event = "VeryLazy",
+    config = function()
+      require("supermaven-nvim").setup({
+        disable_inline_completion = true,
+        disable_keymaps = true,
+      })
+    end,
   },
 }
