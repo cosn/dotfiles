115da9034316186b18d952f9ef150b953ed5dc01
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-05 14:21:36 -0700

    update trouble keymaps and set javascript runner to vitest

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -91,25 +91,40 @@ return {
     "folke/trouble.nvim",
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local trouble = require("trouble")
-      trouble.setup({})
-      vim.keymap.set("n", "<leader>xx", function()
-        trouble.toggle()
-      end)
-      vim.keymap.set("n", "<leader>xd", function()
-        trouble.toggle("document_diagnostics")
-      end)
-      vim.keymap.set("n", "<leader>xq", function()
-        trouble.toggle("quickfix")
-      end)
-      vim.keymap.set("n", "<leader>xl", function()
-        trouble.toggle("loclist")
-      end)
-      vim.keymap.set("n", "gR", function()
-        trouble.toggle("lsp_references")
-      end)
-    end,
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
   {
     "vim-test/vim-test",
@@ -124,6 +139,7 @@ return {
       vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
       vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
       vim.cmd("let test#strategy = 'vimux'")
+      vim.cmd("let test#javascript#runner = 'vitest'")
     end,
   },
 }
