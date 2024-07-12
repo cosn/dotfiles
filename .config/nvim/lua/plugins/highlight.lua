dc6a69e738f97afa398c95085b2a89cb075609c9
Cosmin Nicolaescu <cos@cosn.dev>
2024-07-12 16:40:46 -0700

    feat: add indent-blankline

--- .config/nvim/lua/plugins/highlight.lua
+++ .config/nvim/lua/plugins/highlight.lua
@@ -13,4 +13,30 @@ return {
   {
     "prisma/vim-prisma",
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { show_start = false, show_end = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+  },
 }
