fe14b626039fb4df7653b6cbfb46817ee345d841
Cosmin Nicolaescu <cos@cosn.dev>
2024-07-12 16:46:15 -0700

    feat: add mini-icons

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,47 +6,47 @@ return {
     -- Examples: - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
-    require("mini.ai").setup { n_lines = 500 }
+    require("mini.ai").setup({ n_lines = 500 })
 
-    require('mini.basics').setup()
+    require("mini.basics").setup()
 
     -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-bracketed.md
-    require('mini.bracketed').setup()
+    require("mini.bracketed").setup()
 
-    require('mini.bufremove').setup()
+    require("mini.bufremove").setup()
 
-    local miniclue = require('mini.clue')
-    miniclue.setup {
+    local miniclue = require("mini.clue")
+    miniclue.setup({
       triggers = {
         -- Leader triggers
-        { mode = 'n', keys = '<Leader>' },
-        { mode = 'x', keys = '<Leader>' },
+        { mode = "n", keys = "<Leader>" },
+        { mode = "x", keys = "<Leader>" },
 
         -- Built-in completion
-        { mode = 'i', keys = '<C-x>' },
+        { mode = "i", keys = "<C-x>" },
 
         -- `g` key
-        { mode = 'n', keys = 'g' },
-        { mode = 'x', keys = 'g' },
+        { mode = "n", keys = "g" },
+        { mode = "x", keys = "g" },
 
         -- Marks
-        { mode = 'n', keys = "'" },
-        { mode = 'n', keys = '`' },
-        { mode = 'x', keys = "'" },
-        { mode = 'x', keys = '`' },
+        { mode = "n", keys = "'" },
+        { mode = "n", keys = "`" },
+        { mode = "x", keys = "'" },
+        { mode = "x", keys = "`" },
 
         -- Registers
-        { mode = 'n', keys = '"' },
-        { mode = 'x', keys = '"' },
-        { mode = 'i', keys = '<C-r>' },
-        { mode = 'c', keys = '<C-r>' },
+        { mode = "n", keys = '"' },
+        { mode = "x", keys = '"' },
+        { mode = "i", keys = "<C-r>" },
+        { mode = "c", keys = "<C-r>" },
 
         -- Window commands
-        { mode = 'n', keys = '<C-w>' },
+        { mode = "n", keys = "<C-w>" },
 
         -- `z` key
-        { mode = 'n', keys = 'z' },
-        { mode = 'x', keys = 'z' },
+        { mode = "n", keys = "z" },
+        { mode = "x", keys = "z" },
       },
       clues = {
         -- Enhance this by adding descriptions for <Leader> mapping groups
@@ -57,15 +57,25 @@ return {
         miniclue.gen_clues.windows(),
         miniclue.gen_clues.z(),
       },
-    }
+    })
 
-    require('mini.fuzzy').setup()
+    require("mini.fuzzy").setup()
 
-    require('mini.jump').setup()
+    require("mini.jump").setup()
 
-    require('mini.move').setup()
+    require("mini.icons").setup({
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    })
 
-    require('mini.starter').setup()
+    require("mini.move").setup()
+
+    require("mini.starter").setup()
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
@@ -92,6 +102,5 @@ return {
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require("mini.surround").setup()
-
   end,
 }
