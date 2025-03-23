893f0472402e9a5f311d83ab5d091de8c1c1720e
Cosmin Nicolaescu <cos@accrual.com>
2025-03-23 13:57:40 -0700

    chore: add tmux-open

--- setup.sh
+++ setup.sh
@@ -5,17 +5,16 @@ defaults write -g KeyRepeat -int 1
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
 if [ ! -x "/opt/homebrew/bin/brew" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-for i in `cat brew.list`; do /opt/homebrew/bin/brew install $i; done
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
+brew install `cat brew.list`
 omz reload
 
 stow -v -t ~/ .
