e69cac16d585fa8d61e040eb0771209253fae2aa
Cosmin Nicolaescu <cos@accrual.com>
2025-03-23 10:44:23 -0700

    chore: tweak setup script

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
-defaults write http://com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder AppleShowAllFiles YES
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -x "/opt/homebrew/bin/brew" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 for i in `cat brew.list`; do /opt/homebrew/bin/brew install $i; done
 
@@ -18,4 +23,3 @@ stow -v -t ~/ .
 if [ ! -h "$HOME/Library/Application Support/Claude/claude_desktop_config.json" ]; then
   ln -s "$PWD/claude_desktop_config.json" "$HOME/Library/Application Support/Claude/claude_desktop_config.json"
 fi
-
